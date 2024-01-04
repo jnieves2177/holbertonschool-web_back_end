35474f3ad45410d29994253145c00037e5dc1632
jnieves2177 <jnieves3177@gmail.com>
2024-01-04 13:21:48 -0400

    add

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Working with annotations """
+
+
+def add(a: float, b: float) -> float:
+    """
+    Method: add - a function to add two floats
+    Parameters: a and b are flaots
+    Return: the sum of a and b
+    """
+    return (a + b)
