efea58d73f9fcf721a0b1fa549554e52b5cdf3c8
jnieves2177 <jnieves3177@gmail.com>
2024-01-12 21:13:43 -0400

    fix

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -4,24 +4,11 @@ from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    client = MongoClient()
-    db = client.logs
-    collection = db.nginx
-
-    total = collection.count_documents({})
-    get = collection.count_documents({'method': 'GET'})
-    post = collection.count_documents({'method': 'POST'})
-    put = collection.count_documents({'method': 'PUT'})
-    patch = collection.count_documents({'method': 'PATCH'})
-    delete = collection.count_documents({'method': 'DELETE'})
-    status = collection.count_documents({'method': 'GET', 'path': '/status'})
-    nl = '\n'
-
-    print(f"{total} logs{nl}\
-Methods:{nl}\
-\tmethod GET: {get}{nl}\
-\tmethod POST: {post}{nl}\
-\tmethod PUT: {put}{nl}\
-\tmethod PATCH: {patch}{nl}\
-\tmethod DELETE: {delete}{nl}\
-{status} status check")
+    data = MongoClient().logs.nginx
+    print(f'{data.count_documents({})} logs\nMethods:')
+    print(f'\tmethod GET: {data.count_documents({"method": "GET"})}')
+    print(f'\tmethod POST: {data.count_documents({"method": "POST"})}')
+    print(f'\tmethod PUT: {data.count_documents({"method": "PUT"})}')
+    print(f'\tmethod PATCH: {data.count_documents({"method": "PATCH"})}')
+    print(f'\tmethod DELETE: {data.count_documents({"method": "DELETE"})}')
+    print(f'{data.count_documents({"path": "/status"})} status check')
