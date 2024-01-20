ab43453fb53116150a0953d3f6944af75db38f20
jnieves2177 <jnieves3177@gmail.com>
2024-01-19 21:34:23 -0400

    fix

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,25 +1,25 @@
 // Implement a class named Currency
 export default class Currency {
-    constructor(code, name) {
-      this.code = code;
-      this.name = name;
-    }
-  
-    get code() { return this._code; }
-  
-    set code(newCode) {
-      if (typeof newCode !== 'string') { throw Error('Currency must be a string'); }
-      this._code = newCode;
-    }
-  
-    get name() { return this._name; }
-  
-    set name(newName) {
-      if (typeof newName !== 'string') { throw Error('Name must be a string'); }
-      this._name = newName;
-    }
-  
-    displayFullCurrency() {
-      return `${this.name} (${this.code})`;
-    }
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  get code() { return this._code; }
+
+  set code(newCode) {
+    if (typeof newCode !== 'string') { throw Error('Currency must be a string'); }
+    this._code = newCode;
+  }
+
+  get name() { return this._name; }
+
+  set name(newName) {
+    if (typeof newName !== 'string') { throw Error('Name must be a string'); }
+    this._name = newName;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
 }
