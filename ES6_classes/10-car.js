7198097c8dbc2c639194c06a813fb0250985315d
jnieves2177 <jnieves3177@gmail.com>
2024-01-19 22:25:11 -0400

    fix

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,12 +1,12 @@
 // Car Class
 export default class Car {
-    constructor(brand = undefined, motor = undefined, color = undefined) {
-      this._brand = brand;
-      this._motor = motor;
-      this._color = color;
-    }
-  
-    cloneCar() {
-      return new this.constructor();
-    }
+  constructor(brand = undefined, motor = undefined, color = undefined) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    return new this.constructor();
+  }
 }
