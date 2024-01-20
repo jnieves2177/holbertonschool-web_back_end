231750bec7d2f59ddf8c1822096177c82f15a2f4
jnieves2177 <jnieves3177@gmail.com>
2024-01-19 21:24:57 -0400

    fix

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,31 +1,31 @@
 // Implement a class named HolbertonCourse
 export default class HolbertonCourse {
-    constructor(name, length, students) {
-      this._name = name;
-      this._length = length;
-      this._students = students;
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  // Name getter/setter method
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else {
+      this._name = newName;
     }
-  
-    // Name getter/setter method
-    set name(newName) {
-      if (typeof newName !== 'string') {
-        throw new TypeError('Name must be a string');
-      } else {
-        this._name = newName;
-      }
+  }
+
+  get name() { return this._name; }
+ 
+  // Length getter/setter method
+  set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw new TypeError('Length must be a number');
+    } else {
+      this._length = newLength;
     }
-  
-    get name() { return this._name; }
-  
-    // Length getter/setter method
-    set length(newLength) {
-      if (typeof newLength !== 'number') {
-        throw new TypeError('Length must be a number');
-      } else {
-        this._length = newLength;
-      }
-    }
-  
+  }
+
     get length() { return this._length; }
   
     // Students getter/setter method
