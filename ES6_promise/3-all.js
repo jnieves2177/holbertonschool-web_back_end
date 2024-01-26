a17c5ac6ea79cbc0e3d454a52aafc67550dd47f2
jnieves2177 <jnieves3177@gmail.com>
2024-01-26 18:59:31 -0400

    fix

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -6,8 +6,8 @@ export default function handleProfileSignup() {
     uploadPhoto(),
     createUser()])
     .then((messages) => {
-        console.log(
-            ${messages[0].body} ${messages[1].firstName} ${messages[1].lastName}`,
-        );
+      console.log(
+        `${messages[0].body} ${messages[1].firstName} ${messages[1].lastName}`,
+      );
     }).catch(() => console.log('Signup system offline'));
 }
