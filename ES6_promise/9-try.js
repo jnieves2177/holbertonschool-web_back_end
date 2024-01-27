a3b412bc0532a8176678a41102e05c95e458699e
jnieves2177 <jnieves3177@gmail.com>
2024-01-26 21:55:16 -0400

    fix

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,11 +1,11 @@
 // add math results and errors to a queue
 export default function guardrail(mathFunction) {
-    const queue = [];
-    try {
-        queue.push(mathFunction(), 'Guardrail was processed');
-    } catch (error) {
-        queue.push(`Error: ${error.message}`, 'Guardrail was processed');
-    }
+  const queue = [];
+  try {
+    queue.push(mathFunction(), 'Guardrail was processed');
+  } catch (error) {
+    queue.push(`Error: ${error.message}`, 'Guardrail was processed');
+  }
 
-    return queue;
+  return queue;
 }
