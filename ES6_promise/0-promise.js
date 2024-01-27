bdef4f13f4bde2f7c903e31bf4f15bdede287b1b
jnieves2177 <jnieves3177@gmail.com>
2024-01-26 22:27:40 -0400

    fix

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,7 +1,7 @@
 // Return a Promise
 function getResponseFromAPI() {
-    return new Promise((resolve) => {
-        resolve('I did promise');
-    });
+  return new Promise((resolve) => {
+    resolve('I did promise');
+  });
 }
 export default getResponseFromAPI;
