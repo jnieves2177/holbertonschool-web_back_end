bad21d0be33dca5ec26ffd79c5290b8cfb9aea1f
jnieves2177 <jnieves3177@gmail.com>
2024-01-26 21:43:17 -0400

    fix

--- ES6_promise/7-load_balancer.js
+++ ES6_promise/7-load_balancer.js
@@ -1,4 +1,4 @@
 // return the promise that finishes first
 export default function loadBalancer(chinaDownload, USDownload) {
-    return Promise.race([chinaDownload, USDownload]);
+  return Promise.race([chinaDownload, USDownload]);
 }
